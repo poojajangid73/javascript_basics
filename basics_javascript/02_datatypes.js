570b7cb6f7f710bb20c94742318a598cbb04a387
poojajangid73 <poojajangid0u@gmail.com>
2025-08-23 14:34:40 +0530

    datatypes

---
+++ basics_javascript/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" //treat all the js code as newer version
+alert(3+3) //we are using nodejs not browser
+//some datatypes are
+
+//number ->2 to power 53
+//bigint ->use for larger numbers
+//string ->use in either single cots or double cots
+//boolean -> either true or false value
+//null -> standalone value
+//undefined 
+//symbol -> unique values
+//object 
+
+
+
+console.log(typeof number) //undefined
+console.log(typeof bigint) //undefined
+console.log(typeof String) //function
+console.log(typeof boolean) //undefined
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+console.log(typeof symbol) //undefined
+console.log(typeof object) //undefined
