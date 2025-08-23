29010953e3dfaf73d9517b3dc870e69422d00dc8
poojajangid73 <poojajangid0u@gmail.com>
2025-08-23 15:25:35 +0530

    datatype conversion

---
+++ basics_javascript/datatype_conversion.js
@@ -0,0 +1,53 @@
+
+/*let score = 33
+console.log(typeof score) //number
+let marks = "33"
+console.log(typeof marks) //string
+
+//value in converting number to number
+let num = "33"
+let valueInNum = Number(num)
+console.log(typeof valueInNum) //number
+console.log(valueInNum) //33
+
+//value in converting string to number
+let string = "pooja"
+let newNum = Number(string)
+console.log(newNum) //NaN(not a number)
+
+//value in converting boolean to number
+let boole = true
+let create = Number(boole)
+console.log(create) //1 
+//o in the condition of false 
+
+//value in converting null to number
+let data = null
+let value = Number(data)
+console.log(value) //0
+*/
+
+//value in converting undefined to number
+let update = Object
+let valueInUpdate = String(update)
+console.log(valueInUpdate) //0
+
+//conversion of other datatype to number :-
+// "33" -> 33
+// 33 ->33
+// true -> 1
+//false -> 0
+//null -> 0
+//undefined -> NaN
+//symbol -> NaN
+//object -> NaN 
+
+//conversion of other datatype to string
+//33 -> 33
+//true -> true
+//false -> false
+//null -> null
+//undefined ->undefined
+//symbol -> function Symbol() { [native code]}
+//object -> function Object() { [native code]}
+//and same as
