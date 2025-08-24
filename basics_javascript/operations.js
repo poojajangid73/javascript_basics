b59df3adce7c37db9055ec20cf1f6c4e21396110
poojajangid73 <poojajangid0u@gmail.com>
2025-08-24 12:25:08 +0530

    basic operations

---
+++ basics_javascript/operations.js
@@ -0,0 +1,37 @@
+let value = 3
+let negValue = -value
+console.log(negValue) //-3
+
+//some operators are :-
+console.log(2+2) //4
+console.log(2-2)  //0
+console.log(2*2) //4
+console.log(2/2) //1 
+console.log(2**2) //4 (power operator)
+console.log(2%2) //0 (modulo operator)
+ 
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+console.log(str3) //hello hitesh
+
+console.log("1"+2) //12
+console.log(1+"2") //12
+console.log(1+2+"2") //32
+console.log("1"+2+2) //122
+
+console.log(true) //true
+console.log(+true) //1
+ //console.log(true+)  //invalid type
+console.log(+"") //0
+ 
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+pre_fix = ++gameCounter
+post_fix = gameCounter++
+console.log(gameCounter) //102
+console.log(pre_fix) //101
+console.log(gameCounter) //102
+console.log(post_fix) //101
