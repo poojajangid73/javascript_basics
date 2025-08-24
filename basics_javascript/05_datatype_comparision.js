826523a2cc12ccb79589ba1ab6737a6bc8e22d67
poojajangid73 <poojajangid0u@gmail.com>
2025-08-24 12:51:18 +0530

    datatype comparision

---
+++ basics_javascript/05_datatype_comparision.js
@@ -0,0 +1,30 @@
+console.log(2>1) //true
+console.log(2<1) //false
+console.log(2==1) //false
+console.log(2!=1)  //true
+// here are the some basic comparision and so more 
+
+//comparision of different different datatypes are as follows :-
+console.log("2">1) //true 
+console.log(2>"1") //true
+console.log("02">1) //true
+  //avoid to compare different types of datatype .... prefer the samedatatype or use typecasting
+
+console.log(null>0) //false
+console.log(null<0) //false
+console.log(null>=0) //true
+console.log(null<=0) //true
+console.log(null==0) //false
+console.log(null!=0) //true
+
+
+console.log(undefined>0) //false
+console.log(undefined<0) //false
+console.log(undefined>=0) //false
+console.log(undefined<=0) //false
+console.log(undefined==0) //false
+console.log(undefined!=0) //true
+
+//===(triple equal to operator checks the condition as well as the datatype)
+console.log("2" === 2) //false because one is string and the other one is number
+
