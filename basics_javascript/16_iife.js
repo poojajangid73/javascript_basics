f6c767b07cc3f41fef475d81c7a0e705beef6459
poojajangid73 <poojajangid0u@gmail.com>
2025-08-31 13:23:53 +0530

    execution context

--- basics_javascript/16_iife.js
+++ basics_javascript/16_iife.js
@@ -12,3 +12,15 @@
 ((name) => {
     consol.log(`DB connected to ${name}`);
 })('hitesh');
+
+//===========javascript execution and call stack=======
+//context execution is differnt for different environment in javascript
+//javascript is single threaded language
+
+//execution context are of three types-----
+//1. global execution context
+//2. function execution context
+//3. eval execution context
+
+//javascript is executed in two phase 
+//first one is memory phase and second one is execution phase
